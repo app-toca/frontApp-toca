eb9943c05b70f90fe27c494d6ab30e226538e911
Gustavo da Costa <gustavocosta.realitydeveloper@gmail.com>
2022-09-13 22:46:13 -0400

    adding providers in index

--- app_toca/src/providers/index.js
+++ app_toca/src/providers/index.js
@@ -1,1 +1,10 @@
-export const Providers = ({ children }) => {};
+import { CreateSessionProvider } from "./CreateSession";
+import { UsersProvider } from "./Users";
+
+export const Providers = ({ children }) => {
+  return (
+    <CreateSessionProvider>
+      <UsersProvider>{children}</UsersProvider>
+    </CreateSessionProvider>
+  );
+};
