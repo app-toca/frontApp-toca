65774bd475bbe72ec1ff34abeafac1d75f15274b
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-09-14 23:54:10 -0300

    feat: schedule table

--- app_toca/src/components/ModalSchedules/index.js
+++ app_toca/src/components/ModalSchedules/index.js
@@ -1,171 +1,26 @@
-import { useEffect } from "react";
+
 import { Celula, Centered, Column, Container } from "./syles";
 
-const ModalSchedule = ({}) => {
+const ModalSchedule = () => {
   const schedule = [
     { day: 0, hour: "20:00", qtt_users: 3 },
-    { day: 1, hour: "20:00", qtt_users: 3 },
+    { day: 1, hour: "19:00", qtt_users: 1 },
     { day: 2, hour: "20:00", qtt_users: 3 },
     { day: 2, hour: "14:00", qtt_users: 2 },
     { day: 3, hour: "20:00", qtt_users: 3 },
-    { day: 4, hour: "20:00", qtt_users: 3 },
+    { day: 4, hour: "21:00", qtt_users: 4 },
     { day: 5, hour: "20:00", qtt_users: 3 },
-    { day: 6, hour: "20:00", qtt_users: 3 },
+    { day: 6, hour: "22:00", qtt_users: 3 },
   ];
-  let dom = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let seg = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let ter = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let quar = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let quin = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let sex = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
-  let sab = {
-    "9:00": 0,
-    "10:00": 0,
-    "11:00": 0,
-    "12:00": 0,
-    "13:00": 0,
-    "14:00": 0,
-    "15:00": 0,
-    "16:00": 0,
-    "17:00": 0,
-    "18:00": 0,
-    "19:00": 0,
-    "20:00": 0,
-    "21:00": 0,
-    "22:00": 0,
-    "23:00": 0,
-  };
 
-  const newSchedule = (schedule) => {
-    schedule.map((s) => {
-      if (s.day === 0) {
-        dom[s.hour] = s.qtt_users;
-      }
-      if (s.day === 1) {
-        seg[s.hour] = s.qtt_users;
-      }
-      if (s.day === 2) {
-        ter[s.hour] = s.qtt_users;
-      }
-      if (s.day === 3) {
-        quar[s.hour] = s.qtt_users;
-      }
-      if (s.day === 4) {
-        quin[s.hour] = s.qtt_users;
-      }
-      if (s.day === 5) {
-        sex[s.hour] = s.qtt_users;
-      }
-      if (s.day === 6) {
-        sab[s.hour] = s.qtt_users;
-      }
-    });
-  };
-
-  newSchedule();
+  const hours = [{"9:00": 0}, {"10:00": 0}, {"11:00": 0}, {"12:00": 0}, {"13:00": 0}, {"14:00": 0}, {"15:00": 0}, {"16:00": 0}, {"17:00": 0}, {"18:00": 0}, {"19:00": 0}, {"20:00": 0}, {"21:00": 0}, {"22:00": 0}, {"23:00": 0}]
+  const days = [{"0": "Dom"},{"1": "Seg"},{"2": "Ter"},{"3": "Qua"},{"4": "Qui"},{"5": "Sex"}, {"6": "Sab"}]
 
   return (
     <Container>
       <Centered>
-        {/*horários*/}
         <Column>
-          <Celula></Celula>
+          <Celula>Hours</Celula>
           <Celula>9:00</Celula>
           <Celula>10:00</Celula>
           <Celula>11:00</Celula>
@@ -182,141 +37,23 @@ const ModalSchedule = ({}) => {
           <Celula>22:00</Celula>
           <Celula>23:00</Celula>
         </Column>
-        {/*domingo*/}
-        <Column>
-          <Celula>Dom</Celula>
-          {Object.keys(dom).map((d) => {
-            return <Celula>{dom[d]}</Celula>;
+        {days.map((day, index) => {
+
+          const keysD = Object.keys(day)
+          return (
+            <Column key={index}>
+          <Celula>{day[`${index}`]}</Celula>
+          
+          {hours.map((hou) => {
+            const keysH = Object.keys(hou)
+            const qtt = schedule.find((sch) => sch.day === Number(keysD[0]) && sch.hour === keysH[0])
+            return <Celula key={index}>{qtt ? qtt.qtt_users : '-'}</Celula>
+
           })}
-          {/* <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula> */}
+          
         </Column>
-        {/*segunda*/}
-        <Column>
-          <Celula>Seg</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
-        {/*Terça*/}
-        <Column>
-          <Celula>Ter</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
-        {/*quarta*/}
-        <Column>
-          <Celula>Quar</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
-        {/*quinta*/}
-        <Column>
-          <Celula>Quin</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
-        {/*sexta*/}
-        <Column>
-          <Celula>Sex</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
-        {/*sabado*/}
-        <Column>
-          <Celula>Sab</Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-          <Celula></Celula>
-        </Column>
+          )
+        })}
       </Centered>
     </Container>
   );
