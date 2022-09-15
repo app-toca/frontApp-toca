93a04f0740a11cde8ee081084a1b73467d1cef04
GusBTB <gustavocs81@gmail.com>
2022-09-14 22:59:30 -0300

    feat: schedules front

---
+++ app_toca/src/components/ModalSchedules/syles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  top: 8vh;
+  left: 5vw;
+  height: 50vh;
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0px 0px 50px 10px rgba(0, 0, 0, 0.25);
+`;
+
+export const Centered = styled.div`
+  height: 90%;
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Column = styled.div`
+  height: 100%;
+  width: 12.5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const Celula = styled.div`
+  width: 100%;
+  height: 6.25%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+`;
--- app_toca/src/pages/Home/index.js
+++ app_toca/src/pages/Home/index.js
@@ -6,11 +6,13 @@ import { useContext } from "react";
 import { CreateSessionContext } from "../../providers/CreateSession";
 import { useEffect } from "react";
 import { AreasContext } from "../../providers/Areas";
+import ModalSchedule from "../../components/ModalSchedules";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <ModalSchedule />
       <Footer />
     </Container>
   );
